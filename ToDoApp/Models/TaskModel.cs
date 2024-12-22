efb5462f6fa33c4be46b3568f5514375d9d30e4a
TDavydov <timurl1kec0de@gmail.com>
2024-12-22 13:11:02 +0500

    Add project files.

---
+++ ToDoApp/Models/TaskModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoApp.Models
+{
+    public class TaskModel
+    {
+        public bool IsCompleted { get; set; }
+
+        public string Title { get; set; } = null!;
+    }
+}
---
+++ ToDoApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using System.Collections.ObjectModel;
+using ToDoApp.Models;
+
+namespace ToDoApp.ViewModels;
+
+public class MainViewModel : ReactiveObject
+{
+    private string taskTitle = string.Empty;
+    public string TaskTitle
+    {
+        get => taskTitle;
+        set
+        {
+            if (taskTitle != value)
+            {
+                this.RaiseAndSetIfChanged(ref taskTitle, value);
+            }
+        }
+    }
+
+    public ObservableCollection<TaskModel> Tasks { get; set; } = [];
+
+    public MainViewModel()
+    {
+        if (Design.IsDesignMode)
+        {
+            Tasks =
+            [
+                new()
+                {
+                    Title = "Sleep"
+                },
+                new()
+                {
+                    Title = "Code",
+                    IsCompleted = true,
+                },
+            ];
+        }
+    }
+
+    public void CreateTask()
+    {
+        if (string.IsNullOrWhiteSpace(TaskTitle))
+        {
+            return;
+        }
+
+        Tasks.Add(new()
+        {
+            Title = TaskTitle,
+        });
+        TaskTitle = string.Empty;
+    }
+}
